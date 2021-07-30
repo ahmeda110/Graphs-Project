e6f146aa13c3b71a6a39c2d21bec84939c6557e8
Ahmed-Abbas <65793945+ahmeda110@users.noreply.github.com>
2021-07-30 00:03:59 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,29 +5,29 @@ import java.util.Scanner;
 
 class Main {
 
-	static String fileName;	
+  static String fileName;
 
-	@SuppressWarnings("resource")
-	public static void main(String[] args) throws FileNotFoundException {
+  @SuppressWarnings("resource")
+  public static void main(String[] args) throws FileNotFoundException {
 
-		//reading used from my asgmt2
-		Scanner scanner = new Scanner(System.in);		
-		
-		System.out.print("Please input the txt file name you would live to use (must include extension at end, ex: test.txt): ");
-		fileName = scanner.nextLine(); //file to be opened
-		
-		File in = new File (fileName); //checking if file exists
-		if(!in.exists()) { //if not found
-			System.out.println();
-			System.out.println("File not found, terminating. . .");
-			System.exit(0); // terminating
-		}
-		
-		ArrayList <String> info = FileIO.readFile(fileName); //reads file
-		int[][] matrix = AdjacencyMatrixParser.parseFile(info); 
-	
-		DijkstrasAlgorithm.dijkstra(matrix, 0, fileName);	
-		
-		System.out.println("\nOperation complete!");
-	}
+    //reading used from my asgmt2
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Please input the txt file name you would live to use (must include extension at end, ex: test.txt): ");
+    fileName = scanner.nextLine(); //file to be opened
+
+    File in = new File(fileName); //checking if file exists
+    if (! in .exists()) { //if not found
+      System.out.println();
+      System.out.println("File not found, terminating. . .");
+      System.exit(0); // terminating
+    }
+
+    ArrayList < String > info = FileIO.readFile(fileName); //reads file
+    int[][] matrix = AdjacencyMatrixParser.parseFile(info);
+
+    DijkstrasAlgorithm.dijkstra(matrix, 0, fileName);
+
+    System.out.println("\nOperation complete!");
+  }
 }
